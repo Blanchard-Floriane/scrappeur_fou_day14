f297d90ade75b47da743a5e2aa3571ce0cc460f5
LaSnipeuse <floriane.blanchard.pro@gmail.com>
2023-07-13 16:12:56 +0200

    exo dark_trader done without spec part

--- lib/dark_trader.rb
+++ lib/dark_trader.rb
@@ -1,12 +1,49 @@
 require 'rubygems'
 require 'nokogiri'
 require 'open-uri'
+require 'pry'
 
-PAGE_URL = "http://ruby.bastardsbook.com/files/hello-webpage.html"
+PAGE_URL = "https://coinmarketcap.com/all/views/all/"
 
-# set URL to point to where the page exists
-page = Nokogiri::HTML(URI.open(PAGE_URL))
-links = page.css("a")
-puts links.length   # => 6
-puts links[0].text   # => Click here
-puts links[0]["href"] # => http://www.google.com
+@page = Nokogiri::HTML(URI.open(PAGE_URL))
+
+
+def crypto_symbol_method
+  html_elements_crypto_symbols = @page.xpath('//*[@id="__next"]/div[2]/div[2]/div/div[1]/div/div[2]/div[3]/div/table/tbody/tr/td[2]/div/a[1]')
+  
+  crypto_symbol_array = []
+  html_elements_crypto_symbols.each do |element|
+    crypto_symbol_array << element.text # element.text correspond au final à ma crypto_symbol
+  end
+  return crypto_symbol_array
+end
+
+def crypto_price_method
+  html_elements_crypto_prices = @page.xpath('//*[@id="__next"]/div[2]/div[2]/div/div[1]/div/div[2]/div[3]/div/table/tbody/tr/td[5]/div/a/span')
+  
+  crypto_price_array = []
+  html_elements_crypto_prices.each do |element|
+    crypto_price_array << element.text # element.text correspond au final à ma crypto_price
+  end
+  return crypto_price_array
+end
+
+def array_hashes_together
+  crypto_symbol_array = crypto_symbol_method
+  crypto_price_array = crypto_price_method
+
+  result_array = [] #my array that will content all my mini-hashs
+
+  if crypto_symbol_array.length == crypto_price_array.length
+    crypto_symbol_array.each_index do |i|
+      mini_hash = {
+        crypto_symbol_array[i] =>
+        crypto_price_array[i]
+      }
+      result_array << mini_hash
+    end
+  end
+  puts result_array
+end
+
+array_hashes_together
